6cf82bff045c34851a4b7c35b9e49a319eec0519
Neil Sood <soodorama@gmail.com>
2018-10-05 15:40:46 -0700

    added ejs in views

--- Hello_Express/server.js
+++ Hello_Express/server.js
@@ -8,6 +8,28 @@ app.get('/', function(request, response) {
     response.send("Hello Express");
 });
 
+app.get("/users", function (request, response){
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"}, 
+        {name: "Jay", email: "jay@codingdojo.com"}, 
+        {name: "Brendan", email: "brendan@codingdojo.com"}, 
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    response.render('users', {users: users_array});
+})
+
+
 app.listen(8000, function() {
     console.log("listening on 8000");
 });
+
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(__dirname + "/static"));
+// two underscores before dirname
+// try printing out __dirname using console.log to see what it is and why we use it
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
