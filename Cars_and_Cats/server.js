88d62c61c1bcf0509c1792b0b6e5927ca790655f
Neil Sood <soodorama@gmail.com>
2018-10-09 14:57:29 -0700

    updated cars and cats to use EJS

--- Cars_and_Cats/server.js
+++ Cars_and_Cats/server.js
@@ -2,16 +2,17 @@ var express = require("express");
 
 var app = express();
 
-// app.get('/', function(request, response) {
-//     // console.log(request);
-//     // console.log(response);
-//     response.send("Hello Express");
-// });
+app.get('/cars', function(request, response) {
+    response.render('cars')
+})
 
-// app.get("/cars.html", function (request, response){
-//     // response.send()
-// })
+app.get('/cars/new', function(request, response) {
+    response.render('new')
+})
 
+app.get('/cats', function(request, response) {
+    response.render('cats')
+})
 
 app.listen(8000, function() {
     console.log("listening on 8000");
@@ -22,3 +23,8 @@ app.use(express.static(__dirname + "/static"));
 // two underscores before dirname
 // try printing out __dirname using console.log to see what it is and why we use it
 
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
